8936082997e987da1b6adcbde22a919f1533f57e
Anderson <andersoncleyson@protonmail.com>
2022-01-18 11:48:03 -0300

    routes

--- app.js
+++ app.js
@@ -2,12 +2,21 @@ const express = require('express');
 
 const app = express();
 
+const tasks = require('./src/routes/tasks')
+
 const port = 3000;
+
+
+//middleware
+
+app.use(express.json())
 
 //routes
 app.get('/', (req, res) => {
     return res.send('Hello, friend');
 })
+
+app.use('/api/v1/tasks', tasks)
 
 app.listen(port, () => {
     console.log(`Server is running in port ${port}`);
